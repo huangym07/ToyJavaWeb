5dece06cd2e9d55a0dd8d06b747bee6787298b50
huangym07 <987297028@qq.com>
2024-07-06 21:18:00 +0800

    UserTOrderSelectController 完成，用户订单信息的分页查询 需要前端传入 username currentPage 和 pageSize

---
+++ src/com/dlu/train/controller/UserTOrderSelectController.java
@@ -0,0 +1,45 @@
+package com.dlu.train.controller;
+
+import com.alibaba.fastjson.JSON;
+import com.dlu.train.pojo.TOrder;
+import com.dlu.train.service.TOrderService;
+import com.dlu.train.service.TrainService;
+import com.dlu.train.util.ResultMap;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.EOFException;
+import java.io.IOException;
+
+// 用户订单信息查询（分页查询）
+@WebServlet(name = "UserTOrderSelectController")
+public class UserTOrderSelectController extends HttpServlet {
+    ResultMap resultMap = new ResultMap<>();
+    TrainService trainService = new TrainService();
+    TOrderService tOrderService = new TOrderService();
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        doGet(request, response);
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        // 接收参数：username, currentPage, pageSize
+        String username = request.getParameter("username");
+        Integer currentPage = Integer.parseInt(request.getParameter("currentPage"));
+        Integer pageSize = Integer.parseInt(request.getParameter("pageSize"));
+        try {
+            resultMap.setList(tOrderService.selectTOrder(username, currentPage, pageSize));
+            resultMap.setStatus(true);
+        } catch (Exception e) {
+            resultMap.setStatus(false);
+            resultMap.setMessage(e.getMessage());
+        }
+
+        String json = JSON.toJSONString(resultMap);
+        response.setCharacterEncoding("utf-8");
+        response.getWriter().print(json);
+    }
+}
